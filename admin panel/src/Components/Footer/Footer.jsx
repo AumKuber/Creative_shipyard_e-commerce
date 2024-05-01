26c8ed00ed5f3ff0c7a40ccc29e58c78c507c1a1
AumKuber <oklovers4747@gmail.com>
2024-05-01 22:20:07 +0530

    configured fullstack app in single repo

---
+++ admin panel/src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <hr />
+      <p>Copyright @ 2024 - Creative Shipyard.</p>
+    </div>
+  )
+}
+
+export default Footer
